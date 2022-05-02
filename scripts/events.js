9f5e9e8f3be527b5f07227a8bf62bc38a8f39b82
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2022-05-01 21:44:26 -0300

    Finalizando Projeto

--- scripts/events.js
+++ scripts/events.js
@@ -4,11 +4,56 @@ import {
     buttonStop,
     buttonIncrement,
     buttonDecrement,
-    displayMinutes,
-    displaySeconds,
     buttonSound1,
     buttonSound2,
     buttonSound3,
-    buttonSound4
+    buttonSound4,
+    rangeSound
 } from "./elements.js";
 
+export default function Events({controls, timer}){
+
+    buttonPlay.addEventListener('click', function () {
+        controls.play();
+        timer.countDown();
+    })
+    
+    buttonPause.addEventListener('click', function () {
+        controls.pause();
+        timer.hold();
+    })
+    
+    buttonStop.addEventListener('click', function () {
+        controls.stop();
+        timer.reset();
+    })
+    
+    buttonIncrement.addEventListener('click', function () {
+        controls.increment();
+    })
+    
+    buttonDecrement.addEventListener('click', function () {
+        controls.decrement();
+    })
+    
+    buttonSound1.addEventListener('click', function () {
+        controls.sound1();
+    })
+    
+    buttonSound2.addEventListener('click', function () {
+        controls.sound2();
+    })
+    
+    buttonSound3.addEventListener('click', function () {
+        controls.sound3();
+    })
+    
+    buttonSound4.addEventListener('click', function () {
+        controls.sound4();
+    })
+    
+    rangeSound.addEventListener('input', () =>{
+        controls.volumeSound1(rangeSound.value)
+    })
+    
+}
